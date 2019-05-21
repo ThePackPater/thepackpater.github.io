7a168ff1add46d7abe2f7d3ca325d2726e8b3cf6
Andy Pack <thepackpater@gmail.com>
2019-05-21 17:03:47 -0500

    updated modals

--- assets/FE.js
+++ assets/FE.js
@@ -1,13 +1,12 @@
 
     //this triggers the project modal
     $("#projects").on("click", function () {
-        alert("projects button clicked");
       $("#projectModal").on("shown.bs.modal");
     });
     
     // this triggers the skill modal
     $("#skills").on("click", function () {
-        alert("skills button clicked");
+      // alert("skills clicked");
       $("#skillModal").on("shown.bs.modal");
     });
 
